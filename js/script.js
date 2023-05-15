a26e90014b4f2078b1f103fc45cc025ee229c33d
C:\Users\Robik\Desktop\Проекты\React\solmlink <schedamian@gmail.com>
2023-05-15 11:06:56 +0300

    finish first block

--- js/script.js
+++ js/script.js
@@ -10,3 +10,14 @@ const menuActive = () => {
 
 headerBurger.addEventListener('click', menuActive);
 menuClose.addEventListener('click', menuActive);
+
+//ibg
+function ibg() {
+  let ibg = document.querySelectorAll('.ibg');
+  for (var i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector('img')) {
+      ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+    }
+  }
+}
+ibg();
